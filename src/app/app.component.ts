1db8bd80a913d036383347294b9dd2ab46ad5039
YAK0VLEV <yakovlev@uw.edu>
2019-10-05 21:18:28 -0400

    Checks how rebase work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Auction App';
+  title = 'IF GOT IT RIGHT, IMLEMENTatioN of CAROUSEL still should be here!!';
 }
