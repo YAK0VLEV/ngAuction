c80007eb8dfc139101c1c4444db69e5192f55d71
YAK0VLEV <yakovlev@uw.edu>
2019-10-31 23:40:18 -0400

    Implements basic routing, product detail component and fixes CSS issues.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { Error404Component } from './error404/error404.component'
+import { Error404Component } from './error404/error404.component';
 
 
 const routes: Routes = [
